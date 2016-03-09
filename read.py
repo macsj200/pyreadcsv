5190caad9c4f90c28105e4dbcd3b28a15e2b7e61
macsj200 <maxjohansen@berkeley.edu>
2016-03-08 16:09:30 -0800

    add loop

--- read.py
+++ read.py
@@ -7,8 +7,8 @@ contents2 = os.listdir(file_raw_dir)
 
 files = [os.path.join(file_dir,filename) for filename in contents1 if not os.path.isdir(os.path.join(file_dir,filename))] + [os.path.join(file_dir,file_raw_dir) for filename in contents2 if not os.path.isdir(os.path.join(file_dir,file_raw_dir))]
 
-
-with open(files[0]) as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        print(', '.join(row))
+for file in files:
+    with open(files[0], encoding="latin-1") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            print(', '.join(row))
