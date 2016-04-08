72766e5e634a36db3da535a3396e1cb8e33c2184
macsj200 <maxjohansen@berkeley.edu>
2016-04-08 16:13:49 -0700

    stuff

--- read.py
+++ read.py
@@ -1,15 +1,34 @@
-import csv, os, sqlalchemy
+import csv, os
+from dateutil.parser import parse
 file_dir = "/Users/maxjohansen/Downloads/Forecasting Project"
 file_raw_dir = os.path.join(file_dir, "Raw")
 
-contents1 = os.listdir(file_dir)
-contents2 = os.listdir(file_raw_dir)
+contents = os.listdir(file_raw_dir)
 
-files = [os.path.join(file_dir,filename) for filename in contents1 if not os.path.isdir(os.path.join(file_dir,filename))] + [os.path.join(file_dir,file_raw_dir) for filename in contents2 if not os.path.isdir(os.path.join(file_dir,file_raw_dir))]
+files = [os.path.join(file_raw_dir,filename) for filename in contents]
 
-for file in files:
-    with open(files[0], encoding="latin-1") as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            pass
-            # print(', '.join(row))
+with open('vis/data.csv', 'w') as csvoutfile:
+    fieldnames = ['date', 'sum']
+    writer = csv.DictWriter(csvoutfile, fieldnames=fieldnames)
+    # writer.writeheader()
+    for file in files:
+        # Skip 2012 data (for now)
+        if '2012' in file:
+            continue
+        with open(file, encoding="latin-1") as csvfile:
+            the_sum = 0.0
+            reader = csv.reader(csvfile)
+            row_num = 0
+            price_index = -1
+            for row in reader:
+                if row_num == 0:
+                    price_index = [rowname.strip().replace(' ','').lower() for rowname in row].index("invoicetotal")
+                else:
+                    the_sum = the_sum + float(row[price_index])
+                row_num = row_num + 1
+            prettyfilename = file.lower().strip().replace(' ','').replace('_','').replace('ucsf','').replace('.csv.xls','')
+            prettyfilename = prettyfilename[prettyfilename.index('data') + len('data'):]
+            date = parse(prettyfilename)
+            datestring = date.strftime('%Y-%m')
+            print({'date': datestring, 'sum': the_sum})
+            writer.writerow({'date': datestring, 'sum': the_sum})
